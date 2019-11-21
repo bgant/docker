f484814c964b908e726d367740161c7ec6d4e9c9
Brandon Gant <bgant@protonmail.com>
2019-11-20 21:35:35 -0600

    better commands

--- update_and_cleanup.sh
+++ update_and_cleanup.sh
@@ -6,14 +6,15 @@
 # But you cannot remove any of those individual containers
 # You have to rm the service and deploy it again to remove old containers
 
-# Remove any unused networks and volumes
+# Remove any unused local containers, networks, and volumes
+docker container prune --force
 docker network prune --force
-docker volume prune --force
+#docker volume prune --force      # DANGEROUS: What if the wiki container is stopped?
+docker image prune --force --all  # Remove any unused images and dangling images
+#docker system prune --force      # Combines container, network, image, --volume prune in single command
 
-# Remove unused images and update the rest
-docker images | grep -v SIZE | awk -F' ' '{print $3}' | xargs -I {} docker rmi {}
-#docker rmi $(docker images -f "dangling=true" -q)
+# Update images used by running containers
 docker images | grep -v SIZE | awk -F' ' '{print $1":"$2}' | xargs -I {} docker pull {}
 
-# Restart services with latest images
+# Restart services using latest images
 docker service ls | grep -v NAME | awk -F' ' '{print $2}' | xargs -I {} docker service update {}
