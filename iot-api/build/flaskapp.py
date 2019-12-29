6ec05b12f5d1b7b68cf09c1bddb686a87054a16e
Brandon Gant <bgant@protonmail.com>
2019-12-29 16:08:32 -0600

    validated updates work

--- iot-api/build/flaskapp.py
+++ iot-api/build/flaskapp.py
@@ -1,7 +1,8 @@
 # flaskapp.py
 
-from flask import Flask
 import requests
+from flask import Flask
+from authenticate import USER_LIST, CLIENTID_LIST
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +10,14 @@ def index():
     r = requests.get('https://api.thingspeak.com/channels/946198/fields/1/last.txt')
     response = '<h1> The temperature is ' + str(round(float(r.text), 1)) + 'F</h1>'
     return response
+
+@app.route("/update/user=<user>/clientid=<clientid>/field=<int:field>/data=<data>", methods=['GET'])
+def write_data_point(user, clientid, field, data):
+    if (str(user) in USER_LIST and str(clientid) in CLIENTID_LIST):
+        return data
+    else:
+        return "Failed"
+    #return render_template("update.html", data=data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
