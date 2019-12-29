d3954ff7396b31d999869b1e393bf9d062b56198
Brandon Gant <bgant@protonmail.com>
2019-12-29 15:14:23 -0600

    talks to ThingSpeak

--- iot-api/build/flaskapp.py
+++ iot-api/build/flaskapp.py
@@ -1,11 +1,14 @@
 # flaskapp.py
 
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>The temperature is 91.2 F</h1>"
+    r = requests.get('https://api.thingspeak.com/channels/946198/fields/1/last.txt')
+    response = '<h1> The temperature is ' + str(round(float(r.text), 1)) + 'F</h1>'
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
