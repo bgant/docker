573cf86f9691c61922b1d505c61cbc0e84a55f8b
Brandon Gant <bgant@protonmail.com>
2019-12-31 14:16:30 -0600

    cleanup

--- iot-api/build/flaskapp.py
+++ iot-api/build/flaskapp.py
@@ -30,9 +30,11 @@ conn.commit()
 conn.close()
 
 def sql_insert(conn, entities):
+    conn = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES)
     cursorObj = conn.cursor()
     cursorObj.execute('INSERT INTO data(timestamp, user, clientid, field, data) VALUES(?, ? ,?, ?, ?)', entities)
     conn.commit()
+    conn.close()
 
 def sql_last(user, clientid, field):
     conn = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES)
@@ -47,34 +49,15 @@ app = Flask(__name__)
 @app.route("/last/user=<user>&clientid=<clientid>&field=<int:field>", methods=['GET'])
 def index(user, clientid, field):
     last_entry = sql_last(user, clientid, field)
-
-    #c.execute("SELECT data, date_time, MAX(rowid) FROM data WHERE field=?", ('2',))
-    #row2 = c.fetchone()
-    #data1 = str(round((float(row1[0]) * 1.8) + 32))
-    #data2 = str(round((float(row2[0]) * 1.8) + 32))
-    #time_str1 = row1[1]
-    #t1 = dateutil.parser.parse(time_str1)
-    #t_pst1 = t1.astimezone(pytz.timezone('US/Pacific'))
-    #time_stamp1 = t_pst1.strftime('%I:%M:%S %p   %b %d, %Y')
-    #time_str2 = row2[1]
-    #t2 = dateutil.parser.parse(time_str2)
-    #t_pst2 = t2.astimezone(pytz.timezone('US/Pacific'))
-    #time_stamp2 = t_pst2.strftime('%I:%M:%S %p   %b %d, %Y')
-
-    return last_entry
+    return str(last_entry)
 
 @app.route("/update/user=<user>&clientid=<clientid>&field=<int:field>&data=<data>", methods=['GET'])
 def write_data_point(user, clientid, field, data):
     if (str(user) in USER_LIST and str(clientid) in CLIENTID_LIST):
         timestamp = datetime.now(tz=pytz.utc)
         entities = (timestamp, str(user), str(clientid), int(field), round(float(data), 2))
-
-        conn = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES)
         sql_insert(conn, entities)
-        conn.close()
-
-        return data
-
+        return str(data)
     else:
         return "Failed"
 
