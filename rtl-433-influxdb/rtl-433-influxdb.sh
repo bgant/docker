5fc5dbc7be9a106fc9d14cf6d2212fdf20f1ca1e
Brandon Gant <bgant@protonmail.com>
2023-02-18 16:50:11 -0600

    added Source

--- rtl-433-influxdb/rtl-433-influxdb.sh
+++ rtl-433-influxdb/rtl-433-influxdb.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# Source: https://github.com/merbanan/rtl_433/blob/master/docs/INTEGRATION.md
 rtl_433 -f 915M -R 76 -s 250K -F "influx://${INFLUXDB_HOST}:${INFLUXDB_PORT}/write?db=${INFLUXDB_DATABASE}&u=${INFLUXDB_USERNAME}&p=${INFLUXDB_PASSWORD}" -M time:unix:usec:utc
