808073fd5f1dcacb96e0a29d5df059daed246063
Brandon Gant <bgant@protonmail.com>
2024-06-03 13:42:36 -0500

    deleted archived containers

--- archive/example-crash/crash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Sleeping for 30 seconds..."
-sleep 30
-exit 1  # Exit with Error (non-zero)
--- archive/ipxe-ubuntu/make-undionly.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /opt/ipxe/src
-make bin/undionly.kpxe EMBED=/custom-pxe/${PXE_FILE}
-cp bin/undionly.kpxe /custom-pxe
