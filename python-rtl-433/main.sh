9647aaae3223781af8918ecd11f37987abaec003
Brandon Gant <bgant@protonmail.com>
2024-11-27 23:07:30 -0600

    migration to new docker server

--- python-rtl-433/main.sh
+++ python-rtl-433/main.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-./rtl-433.sh | python3 pipe-json.py
+./rtl-433.sh | python3 -u pipe-json.py
+
+# docker logs not showing python print? Need -u for unbuffered output
+# Source: http://stackoverflow.com/questions/29663459/ddg#29745541
