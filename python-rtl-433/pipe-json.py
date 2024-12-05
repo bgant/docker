1534a510d93783ead738cbe8ac90f0051e24410e
Brandon Gant <bgant@protonmail.com>
2024-12-05 14:21:48 -0600

    anything it sees can be sent to webdis

--- python-rtl-433/pipe-json.py
+++ python-rtl-433/pipe-json.py
@@ -11,21 +11,34 @@ webdis = WEBDIS()
 # Create a generator function for piped JSON data 
 def create_generator():
     for line in sys.stdin:
-        if "time" in line:
+        if "time" in line:  # First field in any valid JSON reading is "time"
             yield json.loads(line)
+
+def webdis_water_meter(id):
+    webdis.timeseries(key=f"webdis-water-{id}-use",value=item['consumption'])
+    webdis.timeseries(key=f"webdis-water-{id}-backflow",value=item['backflow'])
+    webdis.timeseries(key=f"webdis-water-{id}-leak",value=item['leaknow'])
 
 # Create generator object, access JSON data, and send to Webdis server
 mygenerator = create_generator()
-for item in mygenerator:
-    temp_F = (9 * float(item['temperature_C']))/5 + 32
-    if item['battery_ok']:
-        battery = 'OK'
+for item in mygenerator:  # item is just a dictionary
+    if ("LaCrosse-TX29IT" in item['model']) and ("8" in str(item['id'])):  # My Front Porch Weather Sensor (old slim device)
+        temp_F = (9 * float(item['temperature_C']))/5 + 32
+        if item['battery_ok']:
+            battery = 'OK'
+        else:
+            battery = 'Low'
+        print(f"{item['model']} ID:{item['id']} {temp_F}\u00b0F and {item['humidity']}% humidity (battery {battery}) ")
+        webdis.timeseries(key='webdis-porch-temp',value=temp_F)
+        webdis.timeseries(key='webdis-porch-humidity',value=item['humidity'])
+        webdis.timeseries(key='webdis-porch-battery',value=item['battery_ok'])
+    elif ("LaCrosse-TX35DTHIT" in item['model']) and ("8" in str(item['id'])):  # My Porch Weather Sensor (duplicate data)
+        pass
+    elif "Neptune-R900" in item['model']:
+        print(f"{item['model']} ID:{item['id']} Consumption:{item['consumption']} Backflow:{item['backflow']} LeakNow:{item['leaknow']}")
+        webdis_water_meter(item['id'])
     else:
-        battery = 'Low'
-    print(f"{item['model']} {temp_F}\u00b0F and {item['humidity']}% humidity (battery {battery}) ")
-    webdis.timeseries(key='webdis-porch-temp',value=temp_F)
-    webdis.timeseries(key='webdis-porch-humidity',value=item['humidity'])
-    webdis.timeseries(key='webdis-porch-battery',value=item['battery_ok'])
+        print("Not Sent to Webdis: ", item)
 
 
 # EXAMPLE rtl_433 json:
--- python-rtl-433/rtl-433.sh
+++ python-rtl-433/rtl-433.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-rtl_433 -f 915M -s 250K -R 76 -F json
+# -s 250K is the sampling rate
+# -R 76 only listens for LaCrosse TX29IT
+#rtl_433 -f 915M -s 250K -R 76 -F json
+
+# -f 915MHz is LaCrosse weather devices
+# -f 912MHz is Neptune-R900 Water Meters                         
+# -f 912MHz listens again for Water Meters (915MHz 1/3 of time and 912MHz 2/3 of time)
+# -H 17 hops every 17 seconds between the two frequencies
+#    (LaCrosse is every 36s and water meters every 160s / odd number of seconds that is half LaCrosse)
+# -E hop switches immediately to the next frequency when a message is logged
+# -Y autolevel adjusts the interference sensitivity of the device automatically
+# -F json outputs in JSON format
+rtl_433 -f 915M -f 912M -f 912M -H 17 -E hop -Y autolevel -F json
