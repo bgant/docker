c81c089931c2611468ffb0becce5a611b8800058
Brandon Gant <bgant@protonmail.com>
2025-08-10 19:40:23 -0500

    new z-wave device

---
+++ zwave-js-ui/Aeotec-ZW078-WaterHeater.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# apk add mosquitto-clients
+#
+# Usage: <script> [true|false]
+#
+# View available MQTT Topics using MQTT Explorer (AppImage)
+# Whatever Name and Location you create for the device is what is used to connect to the device
+
+if [[ -z $1 ]]; then
+    echo "Requires Paramater: [true|false]"
+else
+    /usr/bin/mosquitto_pub -h 192.168.7.140 -p 1883 -t 'zwave/Water_Heater/Heater_Switch/37/0/targetValue/set' -m "{\"value\":$1}"
+fi
