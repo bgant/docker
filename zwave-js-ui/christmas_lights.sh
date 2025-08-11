e942a33e1f9feba65c4675b2e78051b3357365dd
Brandon Gant <bgant@protonmail.com>
2025-08-10 19:41:11 -0500

    renamed file

--- zwave-js-ui/christmas_lights.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Usage: <script> [true|false]
-# View available MQTT Topics using MQTT Explorer (AppImage)
-/usr/bin/mosquitto_pub -h 172.17.0.1 -p 1883 -t 'zwave/Christmas_Tree/Power_Outlet/37/0/targetValue/set' -m "{\"value\":$1}"
